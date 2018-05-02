17a956d2c2892f4efb4e6576d329647af88e7cb1
renjinwei <renjwjx@gmail.com>
2018-05-03 07:49:40 +0800

    Initial Commit

---
+++ Bridge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bridge
+//
+//  Created by jinren on 2018/5/3.
+//  Copyright © 2018年 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
